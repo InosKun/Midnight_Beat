fd9247daeb34d165d71c3adb133421b98f0408dd
InosKun <moracastanosmartin@gmail.com>
2024-12-11 10:44:12 +0100

    Cambio de escena con el reloj

--- MIDNIGHT BEAT FPS/Assets/Scripts/Flashlight.cs
+++ MIDNIGHT BEAT FPS/Assets/Scripts/Flashlight.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    [SerializeField] private Light light;
+    [SerializeField] private new Light light;
     // Start is called before the first frame update
 
     // Update is called once per frame
--- MIDNIGHT BEAT FPS/Assets/Scripts/MouseLook.cs
+++ MIDNIGHT BEAT FPS/Assets/Scripts/MouseLook.cs
@@ -4,37 +4,44 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public enum RotationAxes {
+    public enum RotationAxes
+    {
         MouseXandY = 0,
         MouseX = 1,
         MouseY = 2
     }
+
     public RotationAxes axes = RotationAxes.MouseXandY;
-    public float sensitivityHor = 9.0f; // velocidad
-    public float sensitivityVert = 9.0f;
-    public float minPitchAngle = -45.0f; 
-    public float maxPitchAngle = 45.0f;// rango de rotación vertical
+    public float sensitivityHor = 9.0f; // Horizontal sensitivity
+    public float sensitivityVert = 9.0f; // Vertical sensitivity
+    public float minPitchAngle = -45.0f; // Minimum pitch (vertical) angle
+    public float maxPitchAngle = 45.0f;  // Maximum pitch (vertical) angle
 
-    private float pitchAngle = 0; // cabeceo (pitch) actual
+    private float pitchAngle = 0; // Current vertical rotation angle
 
-    // Start is called before the first frame update
-   
-        
-
-    // Update is called once per frame
     void Update()
     {
-        if (axes == RotationAxes.MouseX) {
+        if (axes == RotationAxes.MouseX)
+        {
+            // Horizontal rotation only
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
-        } else {
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            // Vertical rotation only
             pitchAngle -= Input.GetAxis("Mouse Y") * sensitivityVert;
             pitchAngle = Mathf.Clamp(pitchAngle, minPitchAngle, maxPitchAngle);
-            float yawAngle = transform.localEulerAngles.y; // mantener el mismo ángulo de guiñada (yaw)
-            if (axes == RotationAxes.MouseXandY) {
-            yawAngle += Input.GetAxis("Mouse X") * sensitivityHor;
+            transform.localEulerAngles = new Vector3(pitchAngle, transform.localEulerAngles.y, 0);
         }
-        transform.localEulerAngles = new Vector3(pitchAngle, yawAngle, 0);
-        
-    }
+        else if (axes == RotationAxes.MouseXandY)
+        {
+            // Both horizontal and vertical rotation
+            pitchAngle -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            pitchAngle = Mathf.Clamp(pitchAngle, minPitchAngle, maxPitchAngle);
+
+            float yawAngle = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityHor;
+            transform.localEulerAngles = new Vector3(pitchAngle, yawAngle, 0);
+        }
     }
 }
+
